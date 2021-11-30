9fd27a780a2fbec7eaa2f3927b6dc8a7b7a9588b
Dante Lo Monaco <1578592@edu.vaniercollege.qc.ca>
2021-11-29 21:46:17 -0500

    Changed medkit functionality
    Medkits now add health differently and update the health bar. Added some collectibles at the beginning of the tutorial.

--- TheGraveyardShift/Assets/Scripts/FPSController/PlayerController.cs
+++ TheGraveyardShift/Assets/Scripts/FPSController/PlayerController.cs
@@ -393,7 +393,7 @@ public class PlayerController : MonoBehaviour
         {
             if(health < maxHealth)
             {
-                health += (maxHealth / 3);
+                TakeDamage(-(maxHealth / 3));
                 if (health > maxHealth)
                     health = maxHealth;
                 Destroy(other.gameObject);
--- TheGraveyardShift/Assets/Scripts/Misc/Collectible.cs
+++ TheGraveyardShift/Assets/Scripts/Misc/Collectible.cs
@@ -4,26 +4,29 @@ using UnityEngine;
 
 public class Collectible : MonoBehaviour
 {
+    [Header("Percent Spawn Chance")]
+    [SerializeField]private int spawnChance;
 
     void Start()
     {
+        this.gameObject.SetActive(false);
         if(this.gameObject.tag == "Ammo")
         {
             int rand = Random.Range(0, 100);
-            if (rand < 40)
-                this.gameObject.SetActive(false);
+            if (rand < spawnChance)
+                this.gameObject.SetActive(true);
         }
         else if(this.gameObject.tag == "Battery")
         {
             int rand = Random.Range(0, 100);
-            if (rand < 40)
-                this.gameObject.SetActive(false);
+            if (rand < spawnChance)
+                this.gameObject.SetActive(true);
         }
         else if(this.gameObject.tag == "Medkit")
         {
             int rand = Random.Range(0, 100);
-            if (rand < 40)
-                this.gameObject.SetActive(false);
+            if (rand < spawnChance)
+                this.gameObject.SetActive(true);
         }
     }
 
